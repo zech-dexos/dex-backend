7fc40c4f1bb1dd5727aecb6d31e797298d190a15
Zechariah Cozine <zech.dexos@gmail.com>
2026-05-24 16:40:54 -0700

    fix elevenlabs model to turbo v2.5

--- api.py
+++ api.py
@@ -352,7 +352,7 @@ async def haven_tts(req: dict):
             },
             json={
                 "text": text,
-                "model_id": "eleven_monolingual_v1",
+                "model_id": "eleven_turbo_v2_5",
                 "voice_settings": {
                     "stability": 0.5,
                     "similarity_boost": 0.75
